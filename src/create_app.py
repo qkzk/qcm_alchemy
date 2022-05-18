499bee07345916e1712c7b77fa50316eab0b6233
qkzk <qu3nt1n@gmail.com>
2022-05-18 08:11:27 +0200

    setup key for csrf

--- src/create_app.py
+++ src/create_app.py
@@ -31,7 +31,7 @@ app.config["DOWNLOAD_FOLDER"] = DOWNLOAD_FOLDER
 app.config["MAX_CONTENT_LENGTH"] = 16 * 1000 * 1000
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../db/qcm.db"
-app.config["SECRET_KEY"] = secrets.token_urlsafe(16)
+app.config["SECRET_KEY"] = "my random secret key"
 
 csrf = CSRFProtect(app)
 db = SQLAlchemy(app)
