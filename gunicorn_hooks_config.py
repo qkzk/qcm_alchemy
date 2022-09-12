71d8a9f5968a583ba59676b3d7a5d8ce3085d5de
qkzk <qu3nt1n@gmail.com>
2022-09-12 17:34:02 +0200

    gunicorn hook config file

---
+++ gunicorn_hooks_config.py
@@ -0,0 +1,3 @@
+from src import on_starting as src_on_starting
+
+on_starting = src_on_starting
--- server.sh
+++ server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
-gunicorn wsgi:app -w 3 --threads 1 -b 0.0.0.0:8000
+gunicorn wsgi:app -w 3 --threads 1 -b 0.0.0.0:8000 --config gunicorn_hooks_config.py
 # gunicorn wsgi:app --config gunicorn_config.py
