57a75ea24ed103fcf6ff2cda275af3b1c3c6edea
qkzk <qu3nt1n@gmail.com>
2023-06-09 08:27:32 +0200

    add log to gunicorn

--- server.sh
+++ server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
-gunicorn wsgi:app -w 3 --threads 1 -b 0.0.0.0:443 --config gunicorn_hooks_config.py --access-logfile -
+gunicorn wsgi:app -w 3 --threads 1 -b 0.0.0.0:443 --config gunicorn_hooks_config.py --access-logfile /var/log/gunicorn.log
 # gunicorn wsgi:app --config gunicorn_config.py
